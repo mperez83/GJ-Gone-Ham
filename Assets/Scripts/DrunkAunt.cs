de4598089c89bef4c4d4d0874b9f00983f7a976a
Michael Perez <mperez980089@yahoo.com>
2018-12-01 14:21:06 -0800

    Implemented basic mom and drunk aunt movement

---
+++ Assets/Scripts/DrunkAunt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkAunt : MonoBehaviour
+{
+    float deg;
+    public float stumbleSpeed = 100;
+    public float verticalStumbleDistance = 1;
+    public float horizontalStumbleDistance = 1;
+    Vector2 anchorPos;
+
+    void Start()
+    {
+        anchorPos = transform.position;
+    }
+
+    void Update()
+    {
+        deg += stumbleSpeed * Time.deltaTime;
+        if (deg >= 720) deg -= 720;
+
+        float finalSinX = horizontalStumbleDistance * Mathf.Sin(deg * Mathf.Deg2Rad);
+        float finalSinY = verticalStumbleDistance * Mathf.Sin(0.5f * deg * Mathf.Deg2Rad);
+        transform.position = new Vector3(anchorPos.x + finalSinX, anchorPos.y + finalSinY, transform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/Mother.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mother : MonoBehaviour
+{
+    public float yVel;
+
+    public GameObject player;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, yVel);
+    }
+
+    void Update()
+    {
+        //Set X position to player's X position every frame
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        //transform.Translate(Vector2.up * yVel * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            //SceneManager.LoadScene("Mother Ending");
+        }
+    }
+}
