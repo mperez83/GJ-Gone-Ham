5101047c666249b08617f33de3896c9fdaf3a872
Michael Perez <mperez980089@yahoo.com>
2018-12-01 15:20:23 -0800

    Implemented timer text

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     float timeSpent;
+    int displayTime;
+
+    public Text timerText;
+
+
 
     void Start()
     {
@@ -14,5 +20,8 @@ public class GameMaster : MonoBehaviour
     void Update()
     {
         timeSpent += Time.deltaTime;
+        displayTime = (int)Mathf.Floor(timeSpent);
+        timerText.text = displayTime.ToString();
+        timerText.fontSize = 12 + (int)Mathf.Floor(displayTime * 0.1f);
     }
 }
